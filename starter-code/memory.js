e7bdd4556b1d4ed72c187da5bb988701591c1229
IronHKristy <kristynhorton85@gmail.com>
2017-02-09 09:11:19 -0500

    don't look at me. I'm ugly and useless!

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,5 +1,40 @@
 //******************************************************************
 // Game Logic
+var cards = [
+  aquaman="img/aquaman.jpg",
+  batman="img/batman.jpg",
+  captain_america="img/captain-america.jpg",
+  fantastic_four="img/fantastic-four.jpg",
+  flash="img/flash.jpg",
+  green_arrow="img/green_arrow.jpg",
+  green_lantern="img/green_lantern.jpg",
+  ironman="img/ironman.jpg",
+  spiderman="img/spiderman.jpg",
+  superman="img/superman.jpg",
+  the_avengers="img/the-avengers.jpg",
+  thor="img/thor.jpg"
+];
+
+
+function shuffle(cards) {
+  var m = cards.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = cards[m];
+    cards[m] = cards[i];
+    cards[i] = t;
+  }
+
+  return array;
+}
+
+console.log(array);
 //******************************************************************
 
 //******************************************************************
